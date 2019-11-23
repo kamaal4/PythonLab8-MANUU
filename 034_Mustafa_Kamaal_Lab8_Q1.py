478116b63443e4488f2bb2d598a22b9a482886e8
Mustafa Kamaal <mkamaalg4@gmail.com>
2019-11-24 00:34:15 +0530

    updated version

--- 034_Mustafa_Kamaal_Lab8_Q1.py
+++ 034_Mustafa_Kamaal_Lab8_Q1.py
@@ -1,13 +1,12 @@
 f = open("main.txt","r")
-count1=0
-count2=0
-for i in f:
-    i = f.readline() 
-    if(i.isdigit() == True):
-        count1=count1+1
-    count2=count2+1
-print("The number of digits is:")
-print(count1)
-print("The number of characters is:")
-print(count2)
-f.close()
+f = f.readline()
+Digits=Alphabets=0
+for c in f:
+    if c.isdigit():
+        Digits=Digits+1
+    elif c.isalpha():
+        Alphabets=Alphabets+1
+    else:
+        pass
+print("Letters", Alphabets) 
+print("Digits", Digits)
